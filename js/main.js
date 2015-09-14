740e5560de3b8b06e6b5ac4f3faa1bb2927674c0
kazuku higashiguchi <higashiguchi_kazuki@yahoo.co.jp>
2015-09-14 13:56:08 +0900

    add elements

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Hello.
 //
-// This is The Scripts used for ___________ Theme
+// This is The Scripts used for deltacross Kyoto officail website.
 //
 //
 
@@ -52,6 +52,7 @@ function main() {
   	      slideSpeed : 300,
   	      paginationSpeed : 400,
   	      autoHeight : true,
+          autoPlay: 3000,
   	      itemsCustom : [
 				        [0, 1],
 				        [450, 2],
@@ -121,6 +122,65 @@ function main() {
 
     });
 
+    /*====================================
+    scroll animation
+    ======================================*/
+    new WOW().init();
+
+    /*====================================
+    google maps
+    ======================================*/
+
+    var LATITUDE = 35.027774;     // 緯度
+    var LONGITUDE = 135.769667;     // 経度
+    var $map = document.getElementById('map');
+
+    // Googleマップの表示
+    var map = new google.maps.Map($map,{
+        center: new google.maps.LatLng(LATITUDE,LONGITUDE), // 地図の中心点
+        mapTypeId: google.maps.MapTypeId.ROADMAP,           // 地図の表示タイプ
+        zoom: 16                                            // 地図のズームレベル
+    });
+
+    // カスタムマップタイプを設定
+    var myStyledMapType = new google.maps.StyledMapType(
+        [
+            {
+                "stylers": [
+                    {"hue": "#ff1a00"},
+                    {"invert_lightness": true},
+                    {"saturation": -100},
+                    {"lightness": 33},
+                    {"gamma": 0.5}
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [
+                    {"color": "#2D333C"}
+                ]
+            }
+        ]
+    );
+    // カスタムマップタイプの登録
+    map.mapTypes.set('myMapType', myStyledMapType);
+    map.setMapTypeId('myMapType');
+
+    // Googleマップにマーカーを設置
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(LATITUDE,LONGITUDE),   // マーカーを立てる位置
+        map: map    // マーカーを表示させるGoogleマップオブジェクトを指定
+    });
+
+    // 情報ウィンドウを生成する
+    var template = document.getElementById('infoWindowTemplate');
+    var infoWindow = new google.maps.InfoWindow();
+    infoWindow.setContent(template.innerText);
+    // マーカーをクリックで情報ウィンドウを表示を表示させる
+    google.maps.event.addListener(marker, 'click', function(){
+        infoWindow.open(map, marker);
+    });
 
 
 }());
