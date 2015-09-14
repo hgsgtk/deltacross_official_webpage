c5ff24e90392de4fc0aacbe5697b38574b5802ce
kazuku higashiguchi <higashiguchi_kazuki@yahoo.co.jp>
2015-09-14 15:46:45 +0900

    add and delete

--- js/main.js
+++ js/main.js
@@ -65,22 +65,13 @@ function main() {
 				      ],
   	  });
 
-  	  $("#clients").owlCarousel({
+  	  $("#space-slide").owlCarousel({
   	 
-  	      navigation : false, // Show next and prev buttons
+  	      navigation : true, // Show next and prev buttons
   	      slideSpeed : 300,
   	      paginationSpeed : 400,
   	      autoHeight : true,
-  	      itemsCustom : [
-				        [0, 1],
-				        [450, 2],
-				        [600, 2],
-				        [700, 2],
-				        [1000, 4],
-				        [1200, 5],
-				        [1400, 5],
-				        [1600, 5]
-				      ],
+  	      singleItem : true,
   	  });
 
       $("#testimonial").owlCarousel({
